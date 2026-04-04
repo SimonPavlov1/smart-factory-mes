d1e3e75f55b719555bb60e7bf14e4c4b437494b5
simonpavlov <simonpavlov@icloud.com>
2026-04-04 17:29:24 +0500

    Скрипт для создания БД

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,1 @@
-from app.database import Base
-from .product import ProductType, ProductBOM
-from .inventory import Component, Stock, Reservation
-from .production import Order, Item, ItemStage
+from .inventory import Component, Stock
--- init_db.py
+++ init_db.py
@@ -1,13 +1,14 @@
 from app.database import engine, Base
-# Импортируем все модели, чтобы Base знала о них
-from app.models.product import ProductType, ProductBOM
-from app.models.inventory import Component, Stock, Reservation
-from app.models.production import Order, Item, ItemStage
+from app.models.inventory import Component, Stock
+# Импортируй сюда все остальные модели, которые будешь создавать, например:
+# from app.models.production import ProductType, ProductBOM, Order
 
 def init_db():
-    print("Создание таблиц в базе данных...")
+    print("Создаю таблицы в базе данных...")
+    # Эта команда берет все классы, наследующие Base,
+    # и создает для них таблицы в файле, указанном в config.py
     Base.metadata.create_all(bind=engine)
-    print("База данных готова!")
+    print("База данных успешно инициализирована!")
 
 if __name__ == "__main__":
     init_db()
